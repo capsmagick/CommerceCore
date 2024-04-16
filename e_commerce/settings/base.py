1cd29ec23935ffc0fe1155712dd081a69f20af9e
Rahul M U <rmuwork51@gmail.com>
2024-04-17 03:19:38 +0530

    Update

--- e_commerce/settings/base.py
+++ e_commerce/settings/base.py
@@ -162,8 +162,8 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
